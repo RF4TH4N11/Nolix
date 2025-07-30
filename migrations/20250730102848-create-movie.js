b33d14b4da81a28a1f2efda3248c3ca28341612f
yandinp <yandicitizen@gmail.com>
2025-07-30 17:37:24 +0700

    feat: add migrations movies v 1.0 and models movie v 1.0

---
+++ migrations/20250730102848-create-movie.js
@@ -0,0 +1,49 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Movies', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      year: {
+        type: Sequelize.INTEGER
+      },
+      released: {
+        type: Sequelize.DATE
+      },
+      director: {
+        type: Sequelize.STRING
+      },
+      actors: {
+        type: Sequelize.STRING
+      },
+      plot: {
+        type: Sequelize.STRING
+      },
+      imageUrl: {
+        type: Sequelize.STRING
+      },
+      rating: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Movies');
+  }
+};
---
+++ models/movie.js
@@ -0,0 +1,32 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Movie extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Movie.belongsTo(models.User)
+      Movie.belongsTo(models.Category)
+    }
+  }
+  Movie.init({
+    title: DataTypes.STRING,
+    year: DataTypes.INTEGER,
+    released: DataTypes.DATE,
+    director: DataTypes.STRING,
+    actors: DataTypes.STRING,
+    plot: DataTypes.STRING,
+    imageUrl: DataTypes.STRING,
+    rating: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Movie',
+  });
+  return Movie;
+};
